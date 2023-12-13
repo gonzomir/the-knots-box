2c210c72bbe30b25c088b63ca66134a375854f21
Milen Petrinski <gonzo@greatgonzo.net>
2023-12-13 17:14:10 +0200

    WIP: Try to read NMEA sentencies from serial port and display results.
    Probably not reading the right serial port as no data is captured.

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,63 @@
 
+#include <nmeaparser.h>
+#include "HardwareSerial.h"
+
 #include "draw.h"
 
+NMEAParser parser;
+HardwareSerial SerialGPS(1);
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("setup");
+  Serial.begin(9600);
 
   setupDisplay();
 
-  drawStatus("Testing speed display...");
-  delay(1000);
+  drawStatus("Waiting for GPS...");
 
-  clearStatus();
-
-  Serial.println("Draw speed 2.1.");
-  drawSpeed(2.12345);
-  delay(1000);
-
-  Serial.println("Draw speed 1.5.");
-  drawSpeed(1.4681);
-  delay(1000);
-
-  Serial.println("Draw speed 4.5.");
-  drawSpeed(4.4681);
-  delay(1000);
-
-  Serial.println("Draw speed 4.8.");
-  drawSpeed(4.8281);
-  delay(1000);
-
-  Serial.println("Draw speed 5.2.");
-  drawSpeed(5.2);
-  delay(1000);
-
-  Serial.println("Draw speed 12.2.");
-  drawSpeed(12.2);
-  delay(1000);
-
-  drawStatus("Testing completed.");
-
-//  display.powerOff();
-  Serial.println("setup done");
+  SerialGPS.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  char buffer[200];
+
+  String data = "";
+  char character;
+
+  while (SerialGPS.available()) {
+    Serial.println("SerialGPS available.");
+    character = SerialGPS.read();
+    data.concat(character);
+  }
+
+  Serial.println(data);
+
+  if (parser.dispatch(data)) {
+    // Guess the last parsed sentence type.
+    switch(parser.getLastProcessedType()) {
+        // Is it a GPRMC sentence?
+      case NMEAParser::TYPE_GPRMC:
+        // Show speed.
+
+        drawSpeed(parser.last_gprmc.speed_over_ground);
+
+        break;
+      case NMEAParser::TYPE_GPGGA:
+        sprintf(buffer, "Accuracy: %d m; Satellites: %s", parser.last_gpgga.hdop, parser.last_gpgga.satellites_used);
+        drawStatus(buffer);
+        break;
+      case NMEAParser::UNKNOWN:
+      case NMEAParser::TYPE_PLSR2451:
+      case NMEAParser::TYPE_PLSR2452:
+      case NMEAParser::TYPE_PLSR2457:
+      case NMEAParser::TYPE_GPGSV:
+      case NMEAParser::TYPE_GPGSA:
+      case NMEAParser::TYPE_HCHDG:
+      case NMEAParser::TYPE_GPGLL:
+      case NMEAParser::TYPE_GPVTG:
+      case NMEAParser::TYPE_GPTXT:
+        break;
+    }
+  }
+
+  delay(500);
 }
