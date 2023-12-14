fea42aa4c21faaab37c7ac1385a3859f05b70c42
Milen Petrinski <gonzo@greatgonzo.net>
2023-12-14 14:50:23 +0200

    Improve and fix reading NMEA sentences from GPS.
    Use Serial2 for the GPS, remove newline at the end of read sentences,
    other improvements.

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,60 @@
 
+#include <cstdio>
 #include <nmeaparser.h>
-#include "HardwareSerial.h"
 
 #include "draw.h"
 
 NMEAParser parser;
-HardwareSerial SerialGPS(1);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   setupDisplay();
 
   drawStatus("Waiting for GPS...");
 
-  SerialGPS.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
 }
 
 void loop() {
-  char buffer[200];
+  char status[200];
 
   String data = "";
-  char character;
 
-  while (SerialGPS.available()) {
-    Serial.println("SerialGPS available.");
-    character = SerialGPS.read();
-    data.concat(character);
-  }
+  while (Serial2.available()) {
+    data = Serial2.readStringUntil('\n');
+    // Remove trailing newline.
+    data.remove(data.length() -1);
 
-  Serial.println(data);
+    const char * str = data.c_str();
+    if (parser.dispatch(str)) {
+      Serial.println(parser.getLastProcessedType());
 
-  if (parser.dispatch(data)) {
-    // Guess the last parsed sentence type.
-    switch(parser.getLastProcessedType()) {
+      // Guess the last parsed sentence type.
+      switch(parser.getLastProcessedType()) {
         // Is it a GPRMC sentence?
-      case NMEAParser::TYPE_GPRMC:
-        // Show speed.
+        case NMEAParser::TYPE_GPRMC:
+          // Show speed.
+          drawSpeed(parser.last_gprmc.speed_over_ground);
+          Serial.println(parser.last_gprmc.speed_over_ground);
+          break;
+        case NMEAParser::TYPE_GPGGA:
+          sprintf(status, "Sats: %d; Acc: %d m", parser.last_gpgga.satellites_used, parser.last_gpgga.hdop);
+          drawStatus(status);
+          break;
+        case NMEAParser::UNKNOWN:
+        case NMEAParser::TYPE_PLSR2451:
+        case NMEAParser::TYPE_PLSR2452:
+        case NMEAParser::TYPE_PLSR2457:
+        case NMEAParser::TYPE_GPGSV:
+        case NMEAParser::TYPE_GPGSA:
+        case NMEAParser::TYPE_HCHDG:
+        case NMEAParser::TYPE_GPGLL:
+        case NMEAParser::TYPE_GPVTG:
+        case NMEAParser::TYPE_GPTXT:
+          break;
+      }
 
-        drawSpeed(parser.last_gprmc.speed_over_ground);
-
-        break;
-      case NMEAParser::TYPE_GPGGA:
-        sprintf(buffer, "Accuracy: %d m; Satellites: %s", parser.last_gpgga.hdop, parser.last_gpgga.satellites_used);
-        drawStatus(buffer);
-        break;
-      case NMEAParser::UNKNOWN:
-      case NMEAParser::TYPE_PLSR2451:
-      case NMEAParser::TYPE_PLSR2452:
-      case NMEAParser::TYPE_PLSR2457:
-      case NMEAParser::TYPE_GPGSV:
-      case NMEAParser::TYPE_GPGSA:
-      case NMEAParser::TYPE_HCHDG:
-      case NMEAParser::TYPE_GPGLL:
-      case NMEAParser::TYPE_GPVTG:
-      case NMEAParser::TYPE_GPTXT:
-        break;
     }
   }
 
